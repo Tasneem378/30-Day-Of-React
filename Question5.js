c52cc3a5dd3a246028fb0dbf1df349f19fcbd615
Tasneem378 <tasneem378@gmail.com>
2023-08-28 22:00:07 +0500

    Solve Question 5.

---
+++ Question5.js
@@ -0,0 +1,11 @@
+//Question5_Declare an array called mixedDataTypes, put different data types in the array and find the length of the array.
+//The array size should be greater than 5.
+
+// Declare an array with different data types
+const mixedDataTypes = [42, 'Hello', true, { name: 'John', age: 25 }, null, undefined, [1, 2, 3]];
+
+// Find the length of the array
+const arrayLength = mixedDataTypes.length;
+
+console.log('Array:', mixedDataTypes);
+console.log('Array length:', arrayLength);
