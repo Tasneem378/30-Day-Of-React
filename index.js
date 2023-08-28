21a2830ecc88997214741e04e974721b3ec3e605
Tasneem378 <tasneem378@gmail.com>
2023-08-28 17:50:07 +0500

    Resolved

--- index.js
+++ index.js
@@ -3,3 +3,9 @@
 const arr = Array()
 
 console.log(arr) 
+//One Way Declare empty array
+let array = [];
+//2nd Way You can use the Array() constructor to create an empty array.
+const myArray = new Array();
+//3rd Way You can use the Array.from() method to create an empty array.
+const myarray = array.from([]);
