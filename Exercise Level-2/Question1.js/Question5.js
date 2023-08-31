f41141ac8ffbda26e8dc12ec0730f43a69a16a88
Tasneem378 <tasneem378@gmail.com>
2023-08-31 13:50:15 +0500

    Solved Exercise Level-2

---
+++ Exercise Level-2/Question1.js/Question5.js
@@ -0,0 +1,12 @@
+//Question5_In the webTechs array check if Sass exists in the array and if it exists print 'Sass 
+//is a CSS preprocess'. If it does not exist add Sass to the array and print the array
+
+let webTechs = ['HTML', 'CSS', 'JavaScript'];
+
+if (webTechs.includes('Sass')) {
+  console.log('Sass is a CSS preprocess');
+} else {
+  webTechs.push('Sass');
+  console.log('Added Sass to the array');
+  console.log(webTechs);
+}
